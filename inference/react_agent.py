39ca16abf4bc45ee7d285ae52f8f2403e36283d7
Lorne <dev@luotao.net>
2025-10-08 00:51:01 +1100

    run with openrouter model

--- inference/react_agent.py
+++ inference/react_agent.py
@@ -57,9 +57,12 @@ class MultiTurnReactAgent(FnCallAgent):
         return "<think>" in content and "</think>" in content
     
     def call_server(self, msgs, planning_port, max_tries=10):
-        
-        openai_api_key = "EMPTY"
-        openai_api_base = f"http://127.0.0.1:{planning_port}/v1"
+
+        openai_api_key = (
+            "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"
+        )
+        openai_api_base = f"https://openrouter.ai/api/v1"
+        model_name = "alibaba/tongyi-deepresearch-30b-a3b"
 
         client = OpenAI(
             api_key=openai_api_key,
@@ -72,7 +75,7 @@ class MultiTurnReactAgent(FnCallAgent):
             try:
                 print(f"--- Attempting to call the service, try {attempt + 1}/{max_tries} ---")
                 chat_response = client.chat.completions.create(
-                    model=self.model,
+                    model=model_name,
                     messages=msgs,
                     stop=["\n<tool_response>", "<tool_response>"],
                     temperature=self.llm_generate_cfg.get('temperature', 0.6),
@@ -84,8 +87,8 @@ class MultiTurnReactAgent(FnCallAgent):
                 content = chat_response.choices[0].message.content
 
                 # OpenRouter provides API calling. If you want to use OpenRouter, you need to uncomment line 89 - 90.
-                # reasoning_content = "<think>\n" + chat_response.choices[0].message.reasoning.strip() + "\n</think>"
-                # content = reasoning_content + content                
+                reasoning_content = "<think>\n" + chat_response.choices[0].message.reasoning.strip() + "\n</think>"
+                content = reasoning_content + content
                 
                 if content and content.strip():
                     print("--- Service call successful, received a valid response ---")
@@ -110,6 +113,7 @@ class MultiTurnReactAgent(FnCallAgent):
         return f"vllm server error!!!"
 
     def count_tokens(self, messages):
+        return len(messages)
         tokenizer = AutoTokenizer.from_pretrained(self.llm_local_path) 
         full_prompt = tokenizer.apply_chat_template(messages, tokenize=False)
         tokens = tokenizer(full_prompt, return_tensors="pt")
